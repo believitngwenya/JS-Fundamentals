f9287bfcb7849ea0f00bc34fca3bb6554b1f2562
Believit Ngwenya <believitngwenya@outlook.com>
2025-07-12 14:55:22 +0200

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 
 const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for(let i = 0; i < languages.length; i++) {
+let i = 0; 
+while (i < languages.length) {
   console.log(languages[i]);
+  i++;
 }
